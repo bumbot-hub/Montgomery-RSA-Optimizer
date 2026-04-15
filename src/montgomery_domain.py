01308741b204147e2643ef906cd39fb7ba18c45f
ANOM0S <Carry1@op.pl>
2026-04-15 21:55:54 +0200

    Added comments, and changed the prints

--- src/montgomery_domain.py
+++ src/montgomery_domain.py
@@ -1,18 +1,21 @@
 from typing import Tuple
 
+# Class that handles the logic of the Montgomery domain
 class MontgomeryDomain:
+    # The constructor of the class
     def __init__(self, n: int):
-        self.N = n
-        self.k = self.N.bit_length()
-        self.R = 1 << self.k    # Używamy przesunięcia bitowego zamiast obliczania R logarytmem który zwraca float
-        self.R2 = (self.R**2) % self.N
+        self.N = n                      # The base module
+        self.k = self.N.bit_length()    # The power of the domain module (got from the length of 'n' bits)
+        self.R = 1 << self.k            # calculating the domain module by using bitwise shift 'n' times
+        self.R2 = (self.R**2) % self.N  # Calculating R2 to easier the way to enter the domain
 
-        # Wyliczenie stałych
-        self.r_inv, self.n_prime = self._extended_gdc(self.R, self.N)
+        # Calculation of constants.
+        self.r_inv, self.n_prime = self._extended_gdc(self.R, self.N) # Using extended gdc to obtain R^-1 and N'
 
         self.r_inv = self.r_inv % self.N
         self.n_prime = (-self.n_prime) % self.R
 
+    # The extended GCD* algorithm to obtain R^-1 and N'
     def _extended_gdc(self, a: int, b: int) -> Tuple[int,int]:
         old_r, curr_r = a, b
         old_x, curr_x = 1, 0
@@ -26,16 +29,19 @@ class MontgomeryDomain:
 
         return old_x, old_y
 
+    # Multiplication and reduction of Montgomery
     def redc(self, T: int) -> int:
-        m = ((T & (self.R - 1)) * self.n_prime) & (self.R - 1)  #((T mod R) * n_prime) mod R, gdzie 'mod R' to będzie ucięcie do k-bitów
-        t = (T + (m * self.N)) >> self.k                        #(T + m * N) / R, gdzie dzielenie to przesunięcie bitowe w prawo o k pozycji
+        m = ((T & (self.R - 1)) * self.n_prime) & (self.R - 1)  # ((T mod R) * n_prime) (mod R), where 'mod R' will be cut to k-bits
+        t = (T + (m * self.N)) >> self.k                        # (T + m * N) / R, where the division is a bitwise shift right 'k' times
 
         if t >= self.N:
             return t - self.N
         return t
 
+    # Getting into Montgomery domain
     def to_domain(self, num: int) -> int:
         return self.redc(num * self.R2)
 
+    # Getting out of Montgomery domain
     def from_domain(self, num: int) -> int:
         return self.redc(num)
